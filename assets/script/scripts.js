05e294bbbaa10143196bba5612b12d82f2d783ff
Momolu <lenny0012@hotmail.com>
2021-08-12 23:46:49 -0500

    commit initial

---
+++ assets/script/scripts.js
@@ -0,0 +1,15 @@
+// Variables
+const sections = document.getElementsByClassName("sections");
+const content = document.getElementByClassName("content");
+// const offSetWeigh-content = 
+
+
+// Function
+function scrollSection ()
+{
+    
+    
+}
+
+// Listeneres
+document.addEventListener("click", console.log("Funciona :D"));
