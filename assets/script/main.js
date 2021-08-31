c9161eeffa08f980c10a84dd31227b663ecefb29
Momolu <lenny0012@hotmail.com>
2021-08-31 12:09:47 -0500

    Implementando el diseño resonsivo

---
+++ assets/script/main.js
@@ -0,0 +1,12 @@
+// Variables
+const sections = document.getElementsByClassName("sections");
+const content = document.getElementsByClassName("content");
+const nav = document.getElementById("nav");
+// const offSetWeigh-content = 
+
+
+// Function
+
+console.log(window.innerWidth);
+console.log(content);
+console.log(nav);
