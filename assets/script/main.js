e520b09ced3b324057e89cb6a9b43cf4af9b194d
Momolu <lenny0012@hotmail.com>
2022-04-07 17:56:20 -0500

    vista del componente navbar terminado

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,19 @@
+// Elements
 brandName = document.getElementsByClassName('img-container')[0];
 perfil_img = document.getElementsByClassName('perfil-img')[0];
-console.log(brandName);
 
+// Events
+// hover -> logo
 brandName.children[0].addEventListener('mouseenter', () => {
   perfil_img.classList.remove('hidden');
   perfil_img.classList.add('visible');
-  console.log(perfil_img.classList);
-  console.log('mouse enter');
 });
 brandName.children[0].addEventListener('mouseleave', () => {
   perfil_img.classList.add('hidden');
   perfil_img.classList.remove('visible');
-  console.log('mouse leave');
+});
+
+document.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  console.log(scrollY);
 });
