82145307c33ccf240e68fb379bd711278c5d5aa0
Momolu <lenny0012@hotmail.com>
2022-04-07 21:40:26 -0500

    vista del componente home terminado

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,19 +1,22 @@
 // Elements
 brandName = document.getElementsByClassName('img-container')[0];
 perfil_img = document.getElementsByClassName('perfil-img')[0];
+navbar = document.getElementsByTagName('nav');
 
 // Events
 // hover -> logo
-brandName.children[0].addEventListener('mouseenter', () => {
-  perfil_img.classList.remove('hidden');
-  perfil_img.classList.add('visible');
-});
-brandName.children[0].addEventListener('mouseleave', () => {
-  perfil_img.classList.add('hidden');
-  perfil_img.classList.remove('visible');
-});
+// brandName.children[0].addEventListener('mouseenter', () => {
+//   perfil_img.classList.remove('hidden');
+//   perfil_img.classList.add('visible');
+// });
+// brandName.children[0].addEventListener('mouseleave', () => {
+//   perfil_img.classList.add('hidden');
+//   perfil_img.classList.remove('visible');
+// });
 
-document.addEventListener('scroll', () => {
+document.addEventListener('click', () => {
   const scrollY = window.scrollY;
-  console.log(scrollY);
+  console.log(window);
+  console.log(navbar);
 });
+document.addEventListener('scroll', () => {});
