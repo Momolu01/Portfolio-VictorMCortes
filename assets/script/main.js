eac19a0a79dba50e5a11d0d5f6f3232483eda93a
Momolu <lenny0012@hotmail.com>
2022-04-18 12:57:55 -0500

    reorganizando etiquetas de la seccion skill-work. limpiando y orgando codigo en general.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,45 +1,39 @@
-const links = {
-  portfolio: {
+const links = [
+  {
     name: 'Portafolio',
     link: 'https://portfolio-victor-mc-ortes.vercel.app/',
     tools: ['HTML5', 'CSS', 'JS', 'Frontend', 'UI DESIGN'],
   },
-  todo_list: {
+  {
     name: 'Todo List',
     description:
-      'Todo list es la lista que generalmente usamos para mantener las tareas de día a día con todo lo que tenemos que hacer.',
+      'Todo list es la lista que generalmente usamos para mantener las tareas del día a día con todo lo que tenemos que hacer.',
     link: 'https://todo-list-momolu01.vercel.app/',
     tools: ['HTML5', 'CSS', 'JS', 'REACT.JS'],
     img: ['./../assets/img/todolist-img-0.png'],
   },
-  order_summary: {
+  {
     name: 'Order Summary',
     link: 'https://suspicious-mestorf-8aad8f.netlify.app',
     tools: ['HTML5', 'CSS', 'JS', 'Frontend'],
+    img: ['./../assets/img/order-summary-img-0.png'],
   },
-  faq: {
+  {
     name: 'FAQ Accordion Card',
     link: 'https://faq-accordion-card-momo.netlify.app/',
     tools: ['HTML5', 'CSS', 'JS', 'REACT.JS', 'Frontend'],
   },
-};
+];
 
 // Elements
 filters = document.getElementsByClassName('links');
-perfil_img = document.getElementsByClassName('perfil-img')[0];
-navbar = document.getElementById('nav');
+projects_list = document.getElementsByClassName('views')[0].children[0];
 
 // Events
-// hover
-const hover = (element, action) => {
-  /* param: element::elemento al que se la aplicara una acción, action::acción que se ejecutara*/
-  element.addEventListener('mouseover', action);
-};
-// filters[0].addEventListener('mouserover', (element) => {
-//   element.target.style.transform = 'translate3d(105%, 0, 0)';
-//   console.log('hola');
-// });
 
+const add_project = (project) => {
+  const item_list = document.createElement('li.flex');
+};
 const action = (event) => {
   let before = getComputedStyle(event.target, ':before');
   console.log(before);
@@ -52,9 +46,7 @@ const action = (event) => {
 // });
 
 document.addEventListener('click', (event) => {
-  console.log(links.portfolio);
-  console.log(filters);
-  // console.log(filters, event);
+  console.log(projects_list);
 });
 document.addEventListener('scroll', () => {
   // if (window.scrollY > 0) {
