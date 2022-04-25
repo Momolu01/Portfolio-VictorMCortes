2c33807ddeabda6536096bd46e3196ecf11451a0
Momolu <lenny0012@hotmail.com>
2022-04-25 04:39:20 -0500

    Se agrego: responsive design para tablets, burger_menu. Se realizaron ajustes generales a algunas medidas.

--- assets/script/main.js
+++ assets/script/main.js
@@ -33,8 +33,10 @@ home = document.getElementById('home');
 about_me = document.getElementById('about-me');
 skill_work = document.getElementById('skill-work');
 contact = document.getElementById('contact');
+nav_menu = document.getElementsByClassName('list-menu');
 nav_itemList =
   document.getElementsByClassName('nav-menu')[0].children[1].children;
+burger_icon = document.getElementsByClassName('burger');
 filters = document.getElementsByClassName('links');
 projects_list = document.getElementsByClassName('views')[0].children[0];
 
@@ -89,19 +91,80 @@ const add_project_type2 = (project) => {
             </li>`;
   return item_list;
 };
+const add_project_type3 = (project) => {
+  const item_list = document.createElement('li.flex');
+  item_list.innerHTML = `<li class='flex'>
+              <div class='text-field'>
+                <h3 class='sub-title'>${project.name}</h3>
+                <div class="img-field">
+                  <a href=${project.link} target='_blank'>
+                    <img src=${project.img[0]} alt="">
+                  </a>
+                </div>
+                <p>${project.description}</p>
+                <div class="skills">
+                  <ul class='flex'>
+                    ${project.tools.reduce((previous, current) => {
+                      current = `<li>
+                      <span>${current}</span>
+                    </li>`;
+                      return previous + current;
+                    }, '')}
+                  </ul>
+                </div>
+              </div>
+            </li>`;
+  return item_list;
+};
+const add_aboutme_type1 = () => {
+  about_me.innerHTML = `<section id='about-me' class='flex'>
+  <div class="text-field">
+    <h2 class="title-section ">Sobre Mi</h2>
+    <div class="img-field">
+      <img src="./assets/img/perfil-img-2.jpeg" alt="img-perfil">
+    </div>
+    <p>
+      Soy un desarrollador web front-end del Valle del Cauca, Colombia. Autodidacta que disfruta mucho el explorar y encontrar nuevas soluciones a problemas. 
+    </p>
+  </div>
+</section id='about-me'>`;
+};
 
 // EVENTOS / ALGORITMOS
+
+// Evento: Cambio de componentes
 let contador = 0;
 while (links[contador]) {
-  if (contador % 2 == 0) {
-    projects_list.append(add_project_type1(links[contador]));
+  if (window.innerWidth <= 768) {
+    add_aboutme_type1();
+    projects_list.append(add_project_type3(links[contador]));
   } else {
-    projects_list.append(add_project_type2(links[contador]));
+    if (contador % 2 == 0) {
+      projects_list.append(add_project_type1(links[contador]));
+    } else {
+      projects_list.append(add_project_type2(links[contador]));
+    }
   }
   contador++;
 }
-// STAGES:
 
+// Evento: Burger
+let state_burger = false;
+
+burger_icon[0].addEventListener('click', () => {
+  if (window.innerWidth <= 768) {
+    state_burger = !state_burger;
+    console.log(state_burger);
+    if (state_burger) {
+      nav_menu[0].classList.add('active-burger');
+    } else {
+      nav_menu[0].classList.remove('active-burger');
+    }
+    console.log(typeof nav_menu[0].classList.value);
+  }
+});
+
+// Evento: Stages
 document.addEventListener('scroll', () => {
   const limit = window.scrollY + window.innerHeight / 2;
   const limitHome = home.offsetHeight;
@@ -109,38 +172,40 @@ document.addEventListener('scroll', () => {
   const limitSkill_work = skill_work.offsetHeight + limitAbout_me;
   const limitContact = contact.offsetHeight + limitSkill_work;
   // HOME
-  if (limit < limitHome) {
-    nav_itemList[0].classList.add('active');
-  } else {
-    nav_itemList[0].classList.remove('active');
+  if (window.innerWidth >= 768) {
+    if (limit < limitHome) {
+      nav_itemList[0].classList.add('active');
+    } else {
+      nav_itemList[0].classList.remove('active');
+    }
+    // ABOUT-ME
+    if (limit > limitHome && limit < limitAbout_me) {
+      nav_itemList[1].classList.add('active');
+    } else {
+      nav_itemList[1].classList.remove('active');
+    }
+    // SKILL-WORK
+    if (
+      limit > limitAbout_me &&
+      window.scrollY + (window.innerHeight * 2) / 3 < limitSkill_work
+    ) {
+      nav_itemList[2].classList.add('active');
+    } else {
+      nav_itemList[2].classList.remove('active');
+    }
+    // CONTACT
+    if (
+      window.scrollY + (window.innerHeight * 2) / 3 > limitSkill_work &&
+      window.scrollY + (window.innerHeight * 2) / 3 < limitContact
+    ) {
+      nav_itemList[3].classList.add('active');
+    } else {
+      nav_itemList[3].classList.remove('active');
+    }
   }
-  if (limit > limitHome && limit < limitAbout_me) {
-    nav_itemList[1].classList.add('active');
-  } else {
-    nav_itemList[1].classList.remove('active');
-  }
-  if (
-    limit > limitAbout_me &&
-    window.scrollY + (window.innerHeight * 2) / 3 < limitSkill_work
-  ) {
-    nav_itemList[2].classList.add('active');
-  } else {
-    nav_itemList[2].classList.remove('active');
-  }
-  if (
-    window.scrollY + (window.innerHeight * 2) / 3 > limitSkill_work &&
-    window.scrollY + (window.innerHeight * 2) / 3 < limitContact
-  ) {
-    nav_itemList[3].classList.add('active');
-  } else {
-    nav_itemList[3].classList.remove('active');
-  }
-  console.log('home', limit, home.offsetHeight);
-  console.log('about me', limit, about_me.offsetHeight);
 });
 
-document.addEventListener('click', (e) => {
-  console.log(about_me.offsetTop, home.offsetTop);
-  console.log('window.scrollY', window.innerHeight);
-  console.log(home.offsetHeight / 3 >= window.scrollY);
-});
+// document.addEventListener('click', (e) => {
+//   console.log(nav_menu);
+//   console.log(window.innerWidth <= 768);
+// });
